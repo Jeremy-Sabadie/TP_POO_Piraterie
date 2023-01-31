266ad878b79f0c9d09730afab07c6e332ccb0647
cda07saba <jeremy.sabadie@2isa.org>
2023-01-31 15:48:51 +0100

    Ajoutez des fichiers projet.

---
+++ TP_POO_Piraterie/Navire.cs
@@ -0,0 +1,21 @@
+﻿namespace TP_POO_Piraterie
+{
+    internal class Navire
+    {
+        protected int x;
+        protected int y;
+        protected int flag;
+        protected bool IsDestroy;
+
+        public Navire(int x, int y, int flag, bool isDestroy)
+        {
+            Console.WriteLine("Abcisse x du navire?");
+            this.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("ordonnée y du navire?");
+            this.y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Camp du navire?");
+            this.flag = int.Parse(Console.ReadLine());
+            IsDestroy = false;
+        }
+    }
+}
